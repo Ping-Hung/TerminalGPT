f58832b8231c3aeff87e79f020101c246684a62c
Nick <ko.binghong@gmail.com>
2026-01-25 11:49:50 +0800

    minor updates in LLMClient

--- LLMClient.cs
+++ LLMClient.cs
@@ -2,6 +2,6 @@ using States;
 namespace LLM {
         public interface ILLMClient {
                 Task<bool> connectToLLM(string modelName);
-                Task<string> makeRequest(ConversationState state);
+                Task<string> makeRequest(List<Message> state);
         }
 }
